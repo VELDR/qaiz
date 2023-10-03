75cb6c6e1d66b1b535725021efd5774a063dfda4
VELDR <melvernardell@gmail.com>
2023-10-03 11:48:37 +0700

    change theme toggle

--- src/components/navbar/ThemeToggle.tsx
+++ src/components/navbar/ThemeToggle.tsx
@@ -1,8 +1,7 @@
 'use client';
-
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
-import { Moon, Sun } from 'lucide-react';
 
 const ThemeToggle = ({
   className,
@@ -18,9 +17,9 @@ const ThemeToggle = ({
     <div className={className} {...props}>
       <Button variant="outline" size="icon" onClick={toggleTheme}>
         {theme === 'dark' ? (
-          <Sun className="h-[1.2rem] w-[1.2rem]" />
+          <MoonIcon className=" h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         ) : (
-          <Moon className="h-[1.2rem] w-[1.2rem]" />
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         )}
         <span className="sr-only">Toggle theme</span>
       </Button>
